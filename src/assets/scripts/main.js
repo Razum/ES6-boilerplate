95028834bc53d327d1bd81661a99827cf2a54ba5
Razum <razumyeyenko@gmail.com>
2017-08-06 10:27:54 +0300

    Webpack updated

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,9 +1,8 @@
 console.log('Hello world');
 
-var mySet = new Set([1, 2, 3, 4, 5, 5, 5, 6]);
+let mySet = new Set([1, 2, 3, 4, 5, 5, 5, 6]);
 console.log(mySet);
 
 const [a, b] = [4, 6];
 
 console.log(a, b);
-
